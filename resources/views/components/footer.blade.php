32b2924d25af72ef3ab09c20be98ce3fd07272c9
Nicolo' Gusinu <nicologus93@hotmail.it>
2024-02-24 14:37:30 +0100

    Inizializzato progetto e cominciata home page solo Front end

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,21 @@
+<div class="container-fluid footer">
+    <div class="row">
+        <div class="col-2">
+            <ul>
+                <p class="fw-bold">Informazioni</p>
+                <li>Chi siamo</li>
+                <li>Privacy</li>
+                <li>Sedi</li>
+                <li>Contatti</li>
+            </ul>
+        </div>
+        <div class="col-2">
+            <p class="fw-bold">Cerchi lavoro?</p>
+            <ul class="p-0">
+                <li>Metodi di lavoro</li>
+                <li>Formazione</li>
+                <li class="btn btn-warning d-flex align-items-center">Lavora con noi</li>
+            </ul>
+        </div>
+    </div>
+</div>
